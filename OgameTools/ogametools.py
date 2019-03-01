9eec4c4c7396430902672ac3adbc2558882db61a
taeyongjeon <47409372+taeyongjeon@users.noreply.github.com>
2019-03-01 15:01:34 +0900

    변경 ogametools.py
    테스트케이스 분리

--- OgameTools/ogametools.py
+++ OgameTools/ogametools.py
@@ -9,7 +9,30 @@ from selenium import webdriver
 import time
 
 def OgameTools(URL,loginid,loginpw):
-    # 이 아래부터 테스트케이스
+
+    testcase = 1
+    if testcase == 1:
+        testcasestart(URL,loginid,loginpw)
+        return 0
+    browser = preparewebdriver.prepare_webdriver()
+    loginogame.login_ogame(browser,URL,loginid,loginpw)
+    while(choice!=8):
+        cls()
+        choice = input("작업 입력\n1 : 정찰\n2 : 정찰 내용을 Csv로 저장\n3 : 자동공격\n5 : 갤럭시툴\n8 : 종료\n--> ")
+        if int(choice) == 1:
+            espionage(browser)
+        if int(choice) == 2:
+            mailtocsv.mailtocsv(browser)
+        if int(choice) == 3:
+            autoattack.auto_attack(browser)
+        if int(choice) == 5:
+            enterGalaxyTab(browser)
+        if int(choice) == 8:
+           browser.quit()
+           break
+
+def testcasestart(URL,loginid,loginpw):
+        # 이 아래부터 테스트케이스
     testcase = 1
     if testcase == 1:
         print("테스트 - 시간 측정")
@@ -28,9 +51,9 @@ def OgameTools(URL,loginid,loginpw):
         #print("테스트 - 정찰 시간 : "+str(time.time()-start_time))
         #start_time = time.time()
         
-        mailtocsv.mail_to_csv(browser)
-        print("테스트 - 메일 정리 시간 : "+str(time.time()-start_time))
-        start_time = time.time()
+        #mailtocsv.mail_to_csv(browser)
+        #print("테스트 - 메일 정리 시간 : "+str(time.time()-start_time))
+        #start_time = time.time()
 
         with open('Espionage_2019_test.csv', encoding="utf-8",mode='w') as f: # 테스트케이스입니다.
             print("tst,tst,tst,tst,tst,tst,tst,tst,tst,tst,tst,tst,tst",file=f)
@@ -41,31 +64,14 @@ def OgameTools(URL,loginid,loginpw):
         #print("테스트 - 공격 시간 : "+str(time.time()-start_time))
         #start_time = time.time()
         
-        #galaxytool.enter_galaxy_tab(browser)
-        #print("테스트 - 갤럭시툴 시간 : "+str(time.time()-start_time))
-        #start_time = time.time()
-        
-        return 0
-
-    # 이 위까지 테스트케이스
-    browser = preparewebdriver.prepare_webdriver()
-    loginogame.login_ogame(browser,URL,loginid,loginpw)
-    while(choice!=8):
-        cls()
-        choice = input("작업 입력\n1 : 정찰\n2 : 정찰 내용을 Csv로 저장\n3 : 자동공격\n5 : 갤럭시툴\n8 : 종료\n--> ")
-        if int(choice) == 1:
-            espionage(browser)
-        if int(choice) == 2:
-            mailtocsv.mailtocsv(browser)
-        if int(choice) == 3:
-            autoattack.auto_attack(browser)
-        if int(choice) == 5:
-            enterGalaxyTab(browser)
-        if int(choice) == 8:
-           browser.quit()
-           break
-
+        galaxytool.enter_galaxy_tab(browser)
+        print("테스트 - 갤럭시툴 시간 : "+str(time.time()-start_time))
+        start_time = time.time()
 
 
 if __name__ == '__main__':
-    OgameTools("https://en.ogame.gameforge.com/", "dfo@vomoto.com", "789456")
+    country = input("주소 앞 서브도메인을 입력하십시오.\n예시 : 영국 서버(https://en.ogame.gameforge.com/)라면 en을 입력하십시오.\n--> ")
+    loginid = input("이메일을 입력하십시오.\n--> ")
+    loginpw = input("암호를 입력하십시오.\n--> ")
+    #OgameTools("https://en.ogame.gameforge.com/", "dfo@vomoto.com", "789456")
+    OgameTools("https://"+country+".ogame.gameforge.com/", loginid, loginpw)
