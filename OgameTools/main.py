5be795f3fb282746e4673dea9583cbb7b8eb6a99
taeyongjeon <47409372+taeyongjeon@users.noreply.github.com>
2019-03-14 19:14:30 +0900

    함수명 수정

--- OgameTools/main.py
+++ OgameTools/main.py
@@ -31,7 +31,7 @@ def main(URL,loginid,loginpw):
 
 
 if __name__ == '__main__':
-    country = input("주소 앞 서브도메인을 입력하십시오.\n예시 : 영국 서버(https://en.ogame.gameforge.com/)라면 en을 입력하십시오.\n--> ")
-    loginid = input("이메일을 입력하십시오.\n--> ")
-    loginpw = input("암호를 입력하십시오.\n--> ")
-    OgameTools("https://"+country+".ogame.gameforge.com/", loginid, loginpw)
+    #country = input("주소 앞 서브도메인을 입력하십시오.\n예시 : 영국 서버(https://en.ogame.gameforge.com/)라면 en을 입력하십시오.\n--> ")
+    #loginid = input("이메일을 입력하십시오.\n--> ")
+    #loginpw = input("암호를 입력하십시오.\n--> ")
+    main("https://en.ogame.gameforge.com/", "dfo@vomoto.com", "789456")
