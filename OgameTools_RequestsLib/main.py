0c99a0d2d3b7903746fa2d5f05268646176fdc8f
taeyongjeon <47409372+taeyongjeon@users.noreply.github.com>
2019-03-17 17:25:49 +0900

    main.py 선택 기능 추가

--- OgameTools_RequestsLib/main.py
+++ OgameTools_RequestsLib/main.py
@@ -7,8 +7,24 @@ import requests
 def main():
     with requests.Session() as s:
         login_ogame.login_ogame(s)
-        mail_to_csv.mail_to_csv(s)
-        espionage.espionage(s)
+
+        while True:
+            choice = input("Espionage : 1 \n"
+                           "Mail To Csv : 2 \n"
+                           "Exit : 3 \n")
+
+            try:
+                choice = int(choice)
+            except:
+                continue
+
+            if choice == 1:
+                espionage.espionage(s)
+            elif choice == 2:
+                mail_to_csv.mail_to_csv(s)
+            elif choice == 3:
+                break
+        
 
 if __name__ == "__main__":
     main()
