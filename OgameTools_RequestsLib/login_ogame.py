2cec7839dd026ae8f2c331378c4a2d42a81ce4d5
taeyongjeon <47409372+taeyongjeon@users.noreply.github.com>
2019-03-17 15:10:10 +0900

    추가 login_ogame.py

---
+++ OgameTools_RequestsLib/login_ogame.py
@@ -0,0 +1,103 @@
+import requests
+import re
+
+def login_ogame():
+    with requests.Session() as s:
+        
+        cookies = post_login(s)
+        accounts_info = get_accounts_info(s)
+        
+        playing_servers_info = get_playing_servers_info(s,accounts_info)
+        
+        print("{} {} {} {} {} {}".format("Number","Server".ljust(12),"Username".ljust(25),"Langauge","Online","Total"))
+        
+        for i in range(0,len(playing_servers_info[0])):
+            print("{} {} {} {} {} {}".format(\
+                str(int(i)+1).ljust(6), playing_servers_info[0][i].ljust(12), accounts_info[3][i].ljust(25),\
+                accounts_info[0][i].ljust(8), playing_servers_info[1][i].ljust(6), playing_servers_info[2][i]))
+
+        select_server = int(input("\nSelect Server Number : ")) - 1
+        connect_server_payload = [accounts_info[2][select_server],accounts_info[0][select_server],accounts_info[1][select_server]]
+        
+        post_server(s, cookies, connect_server_payload)
+
+def post_login(s):
+    payload = {'credentials[email]':'dfo@vomoto.com',
+               'credentials[password]':'789456',
+               'autologin':'false',
+               'language':'en',
+               'kid':''}
+    login_request = s.post('https://lobby-api.ogame.gameforge.com/users', data=payload)
+    print(login_request.status_code)
+
+    return login_request.cookies
+
+def get_accounts_info(s):
+    accounts_json = s.get("https://lobby-api.ogame.gameforge.com/users/me/accounts")
+    
+    accounts = accounts_json.text.split("},")
+    
+    language = []
+    server_number = []
+    gameAccountId = []
+    user_name = []
+
+    for i in range(0,len(accounts)):
+        if "language" in accounts[i]:
+            language.append(re.sub(".*language\":\"|\".*","",accounts[i]))
+            
+        if "number" in accounts[i]:
+            server_number.append(re.sub(".*number\":|\}.*","",accounts[i]))
+            
+        if "gameAccountId" in accounts[i]:
+            gameAccountId.append(re.sub(".*gameAccountId\":|,.*","",accounts[i]))
+            
+        if "name" in accounts[i]:
+            user_name.append(re.sub(".*name\":\"|\".*","",accounts[i]))
+    
+    accounts_info = [language,server_number,gameAccountId,user_name]
+
+    return accounts_info
+
+def get_playing_servers_info(s,accounts_info):
+    server_name = []
+    player_count = []
+    players_online = []
+    
+    servers_json = s.get("https://lobby-api.ogame.gameforge.com/servers")
+    servers = servers_json.text.split("},")
+
+    for i in range(0,len(servers)):
+        for j in range(0,len(accounts_info[0])):
+            player_info = [accounts_info[0][j],accounts_info[1][j]]
+            if is_playing_server(player_info, servers[i]):
+                server_name.append(re.sub(".*name\":\"|\".*","",servers[i]))
+                players_online.append(re.sub(".*playersOnline\":|,.*","",servers[i]))
+                player_count.append(re.sub(".*playerCount\":|,.*","",servers[i]))
+                
+    playing_servers_info = [server_name,player_count,players_online]
+    return playing_servers_info
+
+def is_playing_server(player_info,server_info):
+    return "number\":{},".format(player_info[1]) in server_info and "language\":\"{}".format(player_info[0]) in server_info
+
+def post_server(s, cookies, connect_server_payload):
+    game_account_id = connect_server_payload[0]
+    server_language = connect_server_payload[1]
+    server_number = connect_server_payload[2]
+
+    params = {'id': game_account_id,
+              'server[language]': server_language,
+              'server[number]':server_number}
+    url = ("https://lobby-api.ogame.gameforge.com/users/me/loginLink?id={}"
+           "&server[language]={}"
+           "&server[number]={}".format(game_account_id,server_language,server_number))
+
+    res = requests.get(url,params=params,cookies=cookies)
+    result = re.sub(pattern=".*url\":\"|\"}",repl="",string=res.text).replace("\\","")
+
+    connection = s.get(result)
+    print(connection.text)
+
+if __name__ == "__main__":
+    login_ogame()
