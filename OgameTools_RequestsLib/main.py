f7319a0aabdaccba8627a05d51dacb3d1e2721ef
taeyongjeon <47409372+taeyongjeon@users.noreply.github.com>
2019-03-17 16:09:14 +0900

    변경 main.py

--- OgameTools_RequestsLib/main.py
+++ OgameTools_RequestsLib/main.py
@@ -1,5 +1,6 @@
 import login_ogame
 import mail_to_csv
+import espionage
 
 import requests
 
@@ -7,9 +8,7 @@ def main():
     with requests.Session() as s:
         login_ogame.login_ogame(s)
         mail_to_csv.mail_to_csv(s)
-        
-    
-    
+        espionage.espionage(s)
 
 if __name__ == "__main__":
     main()
