909de8c9c5b19de2a5bf179242b57cee52a5a6ed
taeyongjeon <47409372+taeyongjeon@users.noreply.github.com>
2019-03-17 17:26:04 +0900

    테스트케이스 추가

---
+++ OgameTools_RequestsLib/testcase.py
@@ -0,0 +1,35 @@
+import main
+
+from unittest.mock import patch
+import unittest
+import os
+
+class Test(unittest.TestCase):
+
+    def test_main(self):
+
+        with open("espionage.csv",'w',encoding="utf-8") as f:
+            print("8,479,4\n"
+                  "8,481,10\n"
+                  "8,483,12\n"
+                  "8,484,8",file=f)
+        
+        user_input_1 = [
+            "dfo@vomoto.com",
+            "789456",
+            "2",
+            "1",
+            "2",
+            "3"
+        ]
+        
+        with patch('builtins.input', side_effect=user_input_1):
+            test = main.main()
+
+        try:
+            os.remove("espionage.csv")
+        except:
+            pass
+
+if __name__ == '__main__':
+    unittest.main()
