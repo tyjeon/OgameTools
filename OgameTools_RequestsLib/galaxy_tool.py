5dc670c7867f80e1f1fb5ba1103b065d0052a230
taeyongjeon <47409372+taeyongjeon@users.noreply.github.com>
2019-03-25 20:05:57 +0900

    갤럭시툴 파싱 기능 개선

--- OgameTools_RequestsLib/galaxy_tool.py
+++ OgameTools_RequestsLib/galaxy_tool.py
@@ -21,18 +21,20 @@ def galaxy_tool(s):
 
 	
 	filename = set_csv_title_row()
+
 	for i in range(start_galaxy,end_galaxy+1):
-		galaxy_object = []
+		galaxy_text = []
 		for j in range(start_system,end_system+1):
 			print("Scanning {}:{} ...".format(i,j))
 			payload = {'galaxy':i,
 					   'system':j}
 
 			galaxy_requests = s.post(post_url, data=payload)
-			galaxy_object.append(BeautifulSoup(galaxy_requests.text,"html.parser"))
+			galaxy_text.append(galaxy_requests.text)
+
 
 		for j in range(start_system,end_system+1):
-			parse_ogame_galaxy_source(galaxy_object[j-1],i,j,filename)
+			parse_ogame_galaxy_source(galaxy_text[j-1],i,j,filename)
 
 def set_csv_title_row():
 	if not os.path.isdir("GalaxyTool"):
@@ -49,118 +51,147 @@ def set_csv_title_row():
 	filename = "Galaxy_"+year+"_"+month+"_"+day+"_"+hour+"_"+minute+".csv"
 	
 	with open("GalaxyTool/"+filename, encoding="utf-8",mode='a+') as f:
-		print("Gal,Sys,Pla,PlanetName,Moon,UserName,__user_rank,__alliance_name,__alliance_rank,__alliance_member,Vacation,Inactive,LongInactive,Recyclers",file=f)
+		print("Gal,Sys,Pla,PlanetName,Moon,UserName,user_rank,alliance_name,alliance_rank,alliance_member,Vacation,Inactive,LongInactive,Recyclers",file=f)
 
 	return filename
 
-def parse_ogame_galaxy_source(bs_object,galaxy_number,system_number,filename):
+def parse_ogame_galaxy_source(text,galaxy_number,system_number,filename):
 		planet_number = []
 		planet_name = []
 		is_there_moon = []
-		recyclers_for_debris = []
+		debris = []
 		user_name = []
 		user_rank = []
 		alliance_name = []
 		alliance_rank = []
 		alliance_member = []
-		status_vacation = []
-		status_inactive = []
-		status_longinactive = []
-		
-		system_source = bs_object.findAll("tr",{"class":re.compile("^row.*")})
+		#status_vacation = []
+		#status_inactive = []
+		#status_longinactive = []
 
-		for planet_source in system_source:
-				planet_number.append(get_planet_number(planet_source))
-				planet_name.append(get_planet_name(planet_source))
-				is_there_moon.append(get_moon(planet_source))
-				user_name.append(get_user_name(planet_source))
-				user_rank.append(get_user_rank(planet_source))
-				alliance_name.append(get_alliance_name(planet_source))
-				alliance_rank.append(get_alliance_rank(planet_source))
-				alliance_member.append(get_alliance_member(planet_source))
-				status_vacation.append(get_status(planet_source,"status_abbr_vacation"))
-				status_inactive.append(get_status(planet_source,"status_abbr_inactive"))
-				status_longinactive.append(get_status(planet_source,"status_abbr_longinactive"))
-				recyclers_for_debris.append(get_recyclers_for_debris(planet_source))
+		planet_number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+		planet_name=get_planet_name(text)
+		is_there_moon=get_moon(text)
+		player_info=get_player_info(text)
+		user_name=player_info[0]
+		user_rank=player_info[1]
+		alliance_info = get_alliance_info(text)
+		alliance_name=alliance_info[0]
+		alliance_rank=alliance_info[1]
+		alliance_member=alliance_info[2]
+		#status_vacation.append(get_status(text,"status_abbr_vacation"))
+		#status_inactive.append(get_status(text,"status_abbr_inactive"))
+		#status_longinactive.append(get_status(text,"status_abbr_longinactive"))
+		debris=get_debris(text)
 
 		i = 0
-		for i in range(len(planet_name)):
+		for i in range(15):
 				with open("GalaxyTool/"+filename, encoding="utf-8",mode='a+') as f:
 						print(",".join([
 											str(galaxy_number),
 											str(system_number),
-											planet_number[i],
-											planet_name[i],
-											is_there_moon[i],
-											user_name[i],
-											user_rank[i],
-											alliance_name[i],
-											alliance_rank[i],
-											alliance_member[i],
-											status_vacation[i],
-											status_inactive[i],
-											status_longinactive[i],
-											recyclers_for_debris[i],
+											str(planet_number[i]),
+											str(planet_name[i]),
+											str(is_there_moon[i]),
+											str(user_name[i]),
+											str(user_rank[i]),
+											str(alliance_name[i]),
+											str(alliance_rank[i]),
+											str(alliance_member[i]),
+											#status_vacation[i],
+											#status_inactive[i],
+											#status_longinactive[i],
+											str(debris[i]),
 										]),file=f)
 
-def get_planet_number(planet_source):
-	arguments_for_parsing=[planet_source,["td","class","position js_no_action"],"(<td class=\"position js_no_action\">|</td>)"]
-	return util.parse_using_regexp(arguments_for_parsing)
+def get_planet_name(text):
+	planet_name_slot_regexp = re.compile("<td class=\\\\\"planetname.*?td>")
+	planet_name_slot_raw_text = planet_name_slot_regexp.findall(text)
 
-def get_planet_name(planet_source):
-	arguments_for_parsing=[planet_source,["div","id",re.compile("planet\d+")],"(.*textNormal\">|<\/span.*div>)"]
-	return util.parse_using_regexp(arguments_for_parsing)
+	planet_name_regexp = re.compile("<h1>Planet.*?span>")
+	planet_name_raw_text = planet_name_regexp.findall(text)
+	planet_name = []
+	j=0
 
-def get_moon(planet_source):
-	arguments_for_parsing=[planet_source,["div","class",re.compile("moon_a")],""]
-	is_there_moon_in_html = util.parse_using_regexp(arguments_for_parsing)
-	if "moon" in is_there_moon_in_html:
-		moon_status = "1"
-	else:
-		moon_status = ""
+	for i in range(15):
+		if "js_planetNameEmpty" in planet_name_slot_raw_text[i]:
+			planet_name.append("")
+		else:
+			planet_name.append(re.sub(".*textNormal..>|<.*","",planet_name_raw_text[j]))
+			j+=1
 
-	return moon_status
+	return planet_name
 
-def get_user_name(planet_source):
-	arguments_for_parsing=[planetsource,["div","id",re.compile("player\d+")],".*: <span>|<\/span>.*"]
-	return util.parse_using_regexp(arguments_for_parsing)
+def get_moon(text):
+	moon_regexp = re.compile("<td class=\\\\\"moon.*?td>")
+	moon_raw_text = moon_regexp.findall(text)
+	moon = []
 
-def get_user_rank(planet_source):
-	arguments_for_parsing=[planet_source,["div","id",re.compile("player\d+")],"(.*searchRelId=\d+\">|<\/a><\/li>.*)"]
-	user_rank_in_html = util.parse_using_regexp(arguments_for_parsing)
+	for i in range(15):
+		if "js_no_action" in moon_raw_text[i]:
+			moon.append("")
+		else:
+			moon.append(1)
+	return moon
 
-	if "page=support" in user_rank_in_html: # 관리자 예외
-		user_rank = ""
-	elif "sendMail" in user_rank_in_html: # 밴 당한 사람은 랭크가 없음.
-		user_rank = ""
-	else:
-		user_rank = user_rank_in_html
+def get_debris(text):
+	debris_regexp = re.compile("<td class=\\\\\"debris.*?td>")
+	debris_raw_text = debris_regexp.findall(text)
+	debris = []
 
-	return user_rank
+	for i in range(15):
+		if "js_no_action" in debris_raw_text[i]: # 데브리 존재. get_moon과 반대
+			metal_in_debris = re.compile("(?<=Metal: )[\d\.]+").search(debris_raw_text[i]).group().replace(".","")
+			crystal_in_debris = re.compile("(?<=Crystal: )[\d\.]+").search(debris_raw_text[i]).group().replace(".","")
+			total_debris = int(metal_in_debris)+int(crystal_in_debris)
+			debris.append(total_debris) 
+		else:
+			debris.append("")
 
-def get_alliance_name(planet_source):
-	arguments_for_parsing=[planet_source,["div","id",re.compile("alliance\d+")],"(.*<h1>|<\/h1>.*)"]
-	return util.parse_using_regexp(arguments_for_parsing)
+	return debris
 
-def get_alliance_rank(planet_source):
-	arguments_for_parsing=[planet_source,["div","id",re.compile("alliance\d+")],"(.*searchRelId=\d+\">|<\/a><\/li>).*"]
-	return util.parse_using_regexp(arguments_for_parsing)
+def get_player_info(text):
+	player_slot_regexp = re.compile("<td class=..playername.*?td>")
+	player_slot_raw_text = player_slot_regexp.findall(text)
 
-def get_alliance_member(planet_source):
-	arguments_for_parsing=[planet_source,["div","id",re.compile("alliance\d+")],"(.*Member: |</li><li><a href=\"allianceInfo.*)"]
-	return util.parse_using_regexp(arguments_for_parsing)
+	player_name = []
+	player_rank = []
 
-def get_status(planet_source, condition):
-	arguments_for_parsing=[planet_source,["span","class","status"],""]
-	status_in_html = util.parse_using_regexp(arguments_for_parsing)
-	
-	if condition in status_in_html:
-		status = "1"
-	else:
-		status = ""
+	for i in range(15):
+		try:
+			player_name.append(re.compile("(?<=<span>).*?(?=<)").search(player_slot_raw_text[i]).group())
+		except:
+			player_name.append("")
 
-	return status
+		if "Ranking" in player_slot_raw_text[i]:
+			player_rank.append(re.compile("\d+(?=<\\\/a>)").search(player_slot_raw_text[i]).group())
+		else:
+			player_rank.append("")
 
-def get_recyclers_for_debris(planet_source):
-	arguments_for_parsing=[planet_source,["li","class","debris-recyclers"],".*Recyclers needed: |</li>"]
-	return util.parse_using_regexp(arguments_for_parsing)
+	player_info = [player_name, player_rank]
+	return player_info
+
+def get_alliance_info(text):
+	alliance_slot_regexp = re.compile("allytag.*?td>")
+	alliance_slot_raw_text = alliance_slot_regexp.findall(text)
+
+	alliance_regexp = re.compile("div id=..alliance\d+.*?ul>")
+	alliance_raw_text = alliance_regexp.findall(text)
+	alliance_name = []
+	alliance_rank = []
+	alliance_member = []
+	j = 0
+
+	for i in range(15):
+		if "allytagwrapper" in alliance_slot_raw_text[i]:
+			alliance_name.append(re.compile("(?<=h1>).*?(?=<)").search(alliance_raw_text[j]).group())
+			alliance_rank.append(re.compile("\d+(?=<\\\/a>)").search(alliance_raw_text[j]).group())
+			alliance_member.append(re.compile("(?<=Member: )\d+").search(alliance_raw_text[j]).group())
+			j+=1
+		else:
+			alliance_name.append("")
+			alliance_rank.append("")
+			alliance_member.append("")
+
+	alliance_info = [alliance_name, alliance_rank, alliance_member]
+	return alliance_info
