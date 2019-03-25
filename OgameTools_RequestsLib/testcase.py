5d00bf6294f92c449cff50576f75082131f7f2dd
taeyongjeon <47409372+taeyongjeon@users.noreply.github.com>
2019-03-25 20:06:17 +0900

    테스트케이스 변경

--- OgameTools_RequestsLib/testcase.py
+++ OgameTools_RequestsLib/testcase.py
@@ -6,35 +6,35 @@ import os
 
 class Test(unittest.TestCase):
 
-    def test_main(self):
+	def test_main(self):
 
-        with open("espionage.csv",'w',encoding="utf-8") as f:
-            print("8,479,4\n"
-                  "8,481,10\n"
-                  "8,483,12\n"
-                  "8,484,8",file=f)
-        
-        user_input_1 = [
-            "dfo@vomoto.com",
-            "789456",
-            "2",
-            #"1", # 기능 테스트
-            #"2",
-            "3",
-            "1", # 갤럭시툴 좌표 지정.
-            "1",
-            "1",
-            "2",
-            "9"
-        ]
-        
-        with patch('builtins.input', side_effect=user_input_1):
-            test = main.main()
+		with open("espionage.csv",'w',encoding="utf-8") as f:
+			print("8,479,4\n"
+				  "8,481,10\n"
+				  "8,483,12\n"
+				  "8,484,8",file=f)
+		
+		user_input_1 = [
+			"dfo@vomoto.com",
+			"789456",
+			"1",
+			#"1", # 기능 테스트
+			#"2",
+			"3",
+			"1", # 갤럭시툴 좌표 지정.
+			"1",
+			"1",
+			"2",
+			"9"
+		]
+		
+		with patch('builtins.input', side_effect=user_input_1):
+			test = main.main()
 
-        try:
-            os.remove("espionage.csv")
-        except:
-            pass
+		try:
+			os.remove("espionage.csv")
+		except:
+			pass
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
