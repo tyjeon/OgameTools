7ccd6166008833d833aefe66bf95e84cf8ad8d12
taeyongjeon <taeyongjeon24@gmail.com>
2019-04-10 20:26:16 +0900

    카테고리별 대기-실행 스레드 기능 완성. 만세.

--- OgameTools_RequestsLib/thread_wait.py
+++ OgameTools_RequestsLib/thread_wait.py
@@ -1,27 +1,44 @@
 import threading
-import queue
 import time
+import os
 
-def worker(q):
+def worker(invest_type,wait_time):
+	filename = "{}({}).txt".format(threading.current_thread().getName(),invest_type)
+	with open(filename,encoding="utf-8",mode="w+") as f:
+		f.write(invest_type+"\n")
+		f.write(str(wait_time))
+
 	while True:
 		threads = threading.enumerate()
 		previous_index = threading.enumerate().index(threading.current_thread()) - 1
-		
-		if previous_index == 0:
-			print(threads)
-			item = q.get()
-			time.sleep(2)
-			print(item)
+
+		file_list = os.listdir(os.getcwd())
+		file_list_invest_type = [file for file in file_list if file.endswith("({}).txt".format(invest_type))]
+		created_time_of_current_thread = time.ctime(os.path.getctime(filename))
+
+		my_turn = 1
+		for item in file_list_invest_type:
+			print("check",end=" ")
+			print(time.ctime(os.path.getctime(item)))
+			if created_time_of_current_thread > time.ctime(os.path.getctime(item)):
+				my_turn = 0
+				break
+
+
+		if my_turn == 1:
+			print("시작 : 타입 {} {}".format(invest_type,filename))
+			time.sleep(5)
+			os.remove(filename)
 			break
 		else:
 			time.sleep(1)
-			continue
 
-if __name__ == "__main__":
-	q = queue.Queue()
+def main():
+	#lock = threading.Lock()
 	while True:
-		t = threading.Thread(target=worker, args=(q,))
+		invest_type = input("R/B")
+		wait_time = int(input(""))
+		t = threading.Thread(target=worker, args=(invest_type,wait_time,))
 		t.start()
 
-		item = int(input(""))
-		q.put(item)
+main()
