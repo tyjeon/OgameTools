cf1c84f1c7456ff18265f67758bed9e5df54c419
taeyongjeon <taeyongjeon24@gmail.com>
2019-04-30 17:59:57 +0900

    뮤텍스(세마포어 1개)를 통해 스레드 대기 구현.

--- OgameTools_RequestsLib/thread_wait.py
+++ OgameTools_RequestsLib/thread_wait.py
@@ -3,84 +3,87 @@ import time
 import os
 import shutil
 
-def main():
-	lock = threading.Lock()
-	init_temp_directory()
-	while True:
-		invest_type = input("R/B/show/kill")
-		if invest_type == "R" or invest_type == "B":
-			pass
-		elif invest_type == "show":
-			show_thread()
-			continue
-		elif invest_type == "kill":
-			kill_thread(lock)
-			continue
-		else:
-			continue
+class waitClass:
+	def __init__(self):
+		self.make_temp_directory()
+		self.lock = threading.Lock()
+		self.sem = {}
+		for i in range(30):
+			self.sem["R"+str(i+1)] = threading.Semaphore(1)
+			self.sem["B"+str(i+1)] = threading.Semaphore(1)
 
-		wait_time = int(input(""))
-		t = threading.Thread(target=wait_schedule, args=(invest_type,wait_time,lock))
-		t.start()
+	def main(self):
+		while True:
+			invest_type = input("R/B/show/kill")
 
-def init_temp_directory():
-	if os.path.isdir("temp"):
-		shutil.rmtree("temp")
-	os.mkdir("temp")
+			if invest_type == "R" or invest_type == "B":
+				wait_time = int(input(""))
+				semaphore_name = invest_type + "3"
+				t = threading.Thread(target=self.wait_schedule, args=(invest_type,wait_time,self.sem[semaphore_name]))
+				print(t)
+				t.start()
 
-def show_thread():
-	file_list = os.listdir("temp")
-	for i in range(len(file_list)):
-		print("{} : {}".format(str(i+1),file_list[i]))
+			elif invest_type == "show":
+				self.show_thread()
 
-def kill_thread(lock):
-	show_thread()
-	target_index = input("")
-	file_list = os.listdir("temp")
-	with lock:
-		os.remove("temp/{}".format(file_list[int(target_index)-1]))
+			elif invest_type == "kill":
+				self.kill_thread()
 
-def wait_schedule(invest_type,wait_time,lock):
-	filename = "{}({}).txt".format(threading.current_thread().getName(),invest_type)
-	create_file(filename,wait_time)
+	def make_temp_directory(self):
+		try:
+			shutil.rmtree("temp")
+			os.mkdir("temp")
+		except:
+			os.mkdir("temp")
 
-	while True: # 이부분 '컨디션'으로 처리 https://github.com/taeyongjeon/OgameTools/issues/19
-		if is_my_turn(invest_type,filename):
-			print("시작 : 타입 {} {}".format(invest_type,filename))
-			while True:
-				time.sleep(1) # 이부분을 파일과 엮어야 할 듯. Lock 걸어두고.
-				with lock:
-					try:
-						with open("temp/{}".format(filename),encoding="utf-8",mode="r") as f:
-							duration = int(f.read())
-							if duration == 0:
-								os.remove("temp/{}".format(filename))
-								break
-							duration = duration - 1
-						with open("temp/{}".format(filename),encoding="utf-8",mode="w") as f:
-							f.write(str(duration))
-					except:
-						break
-		else:
+	def show_thread(self):
+		file_list = os.listdir("temp")
+		for i in range(len(file_list)):
+			print("{} : {}".format(str(i+1),file_list[i]))
+
+	def kill_thread(self):
+		show_thread()
+		target_index = input("")
+		file_list = os.listdir("temp")
+		with self.lock:
+			os.remove("temp/{}".format(file_list[int(target_index)-1]))
+
+	def wait_schedule(self,invest_type,wait_time,sem):
+		sem.acquire()
+		filename = "{}({}).txt".format(threading.current_thread().getName(),invest_type)
+		file_path = "temp/{}".format(filename)
+
+		self.create_file(filename,wait_time)
+		print("시작 : 타입 {} {}".format(invest_type,filename))
+
+		while True:
+			with self.lock:
+				try:
+					with open(file_path,encoding="utf-8",mode="r") as f:
+						duration = int(f.read())
+						print(duration)
+						duration = duration - 1
+
+					with open(file_path,encoding="utf-8",mode="w") as f:
+						f.write(str(duration))
+				except:
+					sem.release()
+					return
+
 			time.sleep(1)
 
-		if not os.path.isfile("temp/{}".format(filename)):
-			print("중지됨.")
-			return
+			if duration == 0:
+				os.remove(file_path)
+				sem.release()
+				return
+			elif not os.path.isfile(file_path):
+				print("중지됨.")
+				sem.release()
+				return
 
-def create_file(filename,wait_time):
-	with open("temp/{}".format(filename),encoding="utf-8",mode="w+") as f:
-		f.write(str(wait_time))
+	def create_file(self,filename,wait_time):
+		with open("temp/{}".format(filename),encoding="utf-8",mode="w+") as f:
+			f.write(str(wait_time))
 
-def is_my_turn(invest_type,filename):
-	file_list = os.listdir("temp")
-	file_list_invest_type = [file for file in file_list if file.endswith("({}).txt".format(invest_type))]
-	created_time_of_current_thread = time.ctime(os.path.getctime("temp/{}".format(filename)))
-
-	for item in file_list_invest_type:
-		if created_time_of_current_thread > time.ctime(os.path.getctime("temp/{}".format(item))):
-			return False
-
-	return True
-
-main()
+test = waitClass()
+test.main()
